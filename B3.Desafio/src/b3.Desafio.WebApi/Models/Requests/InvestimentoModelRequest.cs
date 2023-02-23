60aeddc2ab47fa037200c54fbebdc386c8dfd200
Bruno <brunogall86@gmail.com>
2023-02-23 13:34:34 -0300

    Ajuste validação do prazo

--- B3.Desafio/src/b3.Desafio.WebApi/Models/Requests/InvestimentoModelRequest.cs
+++ B3.Desafio/src/b3.Desafio.WebApi/Models/Requests/InvestimentoModelRequest.cs
@@ -14,6 +14,8 @@ namespace b3.Desafio.WebApi.Models.Requests
         {
             if (ValorInicial <= decimal.Zero)
                 errors.Add("Valor de investimento deve ser maior que 0.");
+            if (Prazo <= decimal.Zero)
+                errors.Add("O prazo deve ser maior que 0.");
         }
     }
 }
