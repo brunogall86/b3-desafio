663faf8709bbe04d9c08431cc5c299e5cbead987
Bruno <brunogall86@gmail.com>
2023-02-23 13:42:30 -0300

    Readme.mk

---
+++ B3.Desafio/readme.mk
@@ -0,0 +1,8 @@
+## Roteiro
+	## Entrar na pasta src/b3.Desafio.WebApi
+		# Executar a aplicação com comando donet run br.Desafio.WebApi.csproj
+
+	##  Entrar na pasta src/b3.Desafio.WebApp
+		# Executar a aplicação com o comando npm start
+
+		
